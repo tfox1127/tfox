22b36ddb63b2c02263e329e6edeb9d34535e050e
tfox1127 <65046510+tfox1127@users.noreply.github.com>
2021-06-08 16:55:12 -0500

    test

--- app.py
+++ app.py
@@ -9,28 +9,7 @@ ENV = 'dev'
 
 @app.route('/')
 def index():
-
     return render_template('index.html')
-
-#FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB 
-#FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB FBB 
-@app.route("/fbb")
-def fbb(): 
-    if "fbb_user" in session:
-        return render_template('z3_fbb.html')
-    else:
-        return redirect(url_for("fbb_login"))
-
-@app.route("/fbb/login", methods = ["POST", "GET"])
-def fbb_login():
-    if request.method == "POST":
-        fbb_user = request.form["fbb_user"]
-        session['fbb_user'] = fbb_user
-
-        return redirect(url_for("fbb_leaderboard", fbb_user=fbb_user)) #, fbb_team_name = fbb_team_name
-    else:
-        return render_template("z3_login.html")
-
 
 if __name__ == '__main__':
     app.run()
